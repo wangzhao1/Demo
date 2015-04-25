6460be1fb5c8d72e1139b82b3d87e0888239f327
wangzhao <93126555@qq.com>
2015-04-25 15:57:39 +0800

    Initial commit

---
+++ kkk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kkk
+//
+//  Created by wangzhao on 4/25/15.
+//  Copyright (c) 2015 Huaer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kkk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kkk
+//
+//  Created by wangzhao on 4/25/15.
+//  Copyright (c) 2015 Huaer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
